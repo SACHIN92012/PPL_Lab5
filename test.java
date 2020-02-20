14a79f101b21ceb743fb935b4af00df467ea7f35
Umang Jain <jnumang1999@gmail.com>
2020-02-20 23:25:53 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,7 @@
+class Demo
+{
+    public static void main(args[] k)
+    {
+       System.out.println("Lab 5");
+    }
+}
---
+++ test.py
@@ -0,0 +1,7 @@
+import json
+
+x={"name":"","class":[],"class_type":""}
+
+x["class"].extend(x)
+x["class"]["name"]="sachin"
+print(x)
